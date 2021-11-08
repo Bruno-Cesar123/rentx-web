9c9823bb1dc5dafb9b3ae76a6e5f79c64fc7646c
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-08 20:32:21 -0300

    feat: create page dashboard

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "./styles";
+
+
+export function Dashboard() {
+  return (
+    <Container>
+      <marquee>
+        <h1>dashboard</h1>
+      </marquee>
+    </Container>
+  ) 
+}
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes as Router } from 'react-router-dom';
 import { Landing } from '../pages/Landing';
 import { SignIn } from '../pages/SignIn';
 import { SignUp } from '../pages/SignUp';
+import { Dashboard } from '../pages/Dashboard';
 
 export function Routes() {
   return (
@@ -10,6 +11,7 @@ export function Routes() {
         <Route path='/' element={<Landing />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/signup' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
       </Router>
     </BrowserRouter>
   )
