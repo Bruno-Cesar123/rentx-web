cc9ce4fdaca4ca7a222cfe28dfff7ebf26ca6190
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-15 20:10:35 -0300

    feat: create modal-info dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
-import { FiArrowLeft } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
-import { Container, Header } from "./styles";
+import carImg from '../../assets/landing.png';
+
+import { Container, Header, Content, Card, CardSection } from "./styles";
 
 export function Dashboard() {
 
@@ -11,6 +12,70 @@ export function Dashboard() {
         <img src={logoImg} alt="rentex" />
         <h4>Config</h4>
       </Header>
+      <Content>
+        <h2>Seus carros alugados</h2>
+
+        <CardSection>
+          <Card>
+            <img src={carImg} alt="car" />
+            <h3>Audi A4</h3>
+            <p>
+              description of the car
+            </p>
+
+            <a href="/">
+              Mais informações...
+            </a>
+          </Card>
+          <Card>
+            <img src={carImg} alt="car" />
+            <h3>Audi A4</h3>
+            <p>
+              description of the car
+            </p>
+
+            <a href="/">
+              Mais informações...
+            </a>
+          </Card>
+          <Card>
+            <img src={carImg} alt="car" />
+            <h3>Audi A4</h3>
+            <p>
+              description of the car
+            </p>
+
+            <a href="/">
+              Mais informações...
+            </a>
+          </Card>
+
+          <main>
+            <div>
+              <h3>Informações gerais</h3>
+
+              <section>
+                <p>Data de início: <span>15/11/2021</span></p>
+                <p>Data do Fim: <span>15/11/2021</span></p>
+                <p>Retorno previsto: <span>15/11/2021</span></p>
+                <p>Total: <span>R$ 500,00</span></p>
+              </section>
+            </div>
+            <div>
+              <h3>Informações do carro</h3>
+
+              <section>
+                <p>Nome: <span>AUDI A4</span></p>
+                <p>Marca: <span>AUDI</span></p>
+                <p>Descrição: <span>carro de luxo, 4 lugares</span></p>
+                <p>Valor diário: <span>R$ 50,00</span></p>
+                <p>Multa diária: <span>R$ 80,00</span></p>
+                <p>Licença: <span>ABCD-1234</span></p>
+              </section>
+            </div>
+          </main>
+        </CardSection>
+      </Content>
     </Container>
-  ) 
+  )
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  height: 100vh;
+  max-width: 1200px;
+  margin: 0 auto;
+`;
 
 export const Header = styled.header`
   padding: 12px 8px;
   display: flex;
   align-items: center;
   justify-content: space-between;
-  position: relative;
 
   width: 100%;
   background: #FFFFFF;
@@ -15,5 +18,99 @@ export const Header = styled.header`
   -webkit-box-shadow: 0px 8px 2px #FAFAFA;
   -moz-box-shadow: 0px 8px 2px #FAFAFA;
   box-shadow: 0px 8px 2px #FAFAFA;
+`;
 
+export const Card = styled.div`
+  margin-top: 70px;
+  width: 384px;
+  height: 440px;
+
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+
+  background: #FFFFFF;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+
+  img {
+    margin-top: 12px;
+    width: 344px;
+    height: 185px;
+  }
+
+  h3 {
+    font-size: 24px;
+    margin-top: 12px;
+  }
+
+  p {
+    margin-top: 12px;
+  }
+
+  a {
+    margin-top: 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 220px;
+    height: 45px;
+    background: #FF1010;
+    color: #FFFFFF;
+
+    border-radius: 16px;
+    font-size: 18px;
+    letter-spacing: 2px;
+    cursor: pointer;
+      
+    text-decoration: none;
+      
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
+
+export const Content = styled.section`
+  margin-top: 70px;
+  position: relative;
+
+  h2 {
+    font-size: 28px;
+  }
+`;
+
+export const CardSection = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  main {
+    position: absolute;
+    width: 800px;
+    height: 430px;
+    
+    display: flex;
+    flex-direction: column;
+
+    margin: 0 160px;
+    background: #FFFFFF;
+    box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.25);
+
+    div {
+      h3 {
+        margin-top: 16px;
+        font-size: 28px;
+        text-align: center;
+      }
+
+      p {
+        font-size: 20px;
+        margin-top: 8px;
+        margin-left: 12px;
+      }
+    }
+  }
 `;
