9a53bc8e8dc7c94bbbed92d74574665cbb171c62
Bruno Cesar <brunocesar12386@gmail.com>
2021-11-15 15:12:14 -0300

    feat: create header dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,26 +1,16 @@
-import { useEffect } from 'react';
-import { useAuth } from "../../hooks/AuthContext";
-import { api } from '../../services/api';
-import { Container } from "./styles";
+import { FiArrowLeft } from 'react-icons/fi';
 
+import logoImg from '../../assets/logo.svg';
+import { Container, Header } from "./styles";
 
 export function Dashboard() {
-  const { user, refreshToken } = useAuth();
 
-  useEffect(() => {
-    api.get('/rentals/user')
-      .then(response => {
-        console.log(response.data);
-      })
-  }, []);
-
-  console.log(user);
-  
   return (
     <Container>
-      <marquee>
-        <h1>dashboard</h1>
-      </marquee>
+      <Header>
+        <img src={logoImg} alt="rentex" />
+        <h4>Config</h4>
+      </Header>
     </Container>
   ) 
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,3 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.main``;
+
+export const Header = styled.header`
+  padding: 12px 8px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+
+  width: 100%;
+  background: #FFFFFF;
+  height: 100px;
+  -webkit-box-shadow: 0px 8px 2px #FAFAFA;
+  -moz-box-shadow: 0px 8px 2px #FAFAFA;
+  box-shadow: 0px 8px 2px #FAFAFA;
+
+`;
